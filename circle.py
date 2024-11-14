85b71648a4d9ec8e563b71178eab20ea0938a5e4
Artem Ibragimov <65608749+ttema4@users.noreply.github.com>
2024-11-14 19:35:55 +0300

    Исправления функции периметра, добавлена документация.

--- circle.py
+++ circle.py
@@ -2,9 +2,28 @@ import math
 
 
 def area(r):
+    """
+    Вычисляет площадь круга.
+
+    Параметры:
+    r (float): Радиус круга.
+
+    Пример:
+    >>> area(5)
+    78.53981633974483
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
-    return 2 * math.pi * r
+    """
+    Вычисляет периметр круга.
 
+    Параметры:
+    r (float): Радиус круга.
+
+    Пример:
+    >>> perimeter(5)
+    31.41592653589793
+    """
+    return 2 * math.pi * r
